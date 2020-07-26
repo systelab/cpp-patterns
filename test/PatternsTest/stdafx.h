1e657cb557061c2db5c33943aedbe0f44dc2c2b9
josepvilabadillo <jvila1@werfen.com>
2020-07-26 12:47:14 +0200

    Added project for the unit tests

---
+++ test/PatternsTest/stdafx.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING  1
+
+// STL
+#include <functional>
+#include <stdio.h>
+#include <map>
+#include <memory>
+#include <string>
+
+// GTEST
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
