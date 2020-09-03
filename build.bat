6c4db97573d01042d69522de105632c01091ea39
josepvilabadillo <jvila1@werfen.com>
2020-09-03 12:37:57 +0200

    Update build.bat
    Updated manual build batch file to only build gtest if needed.

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 md build
 cd build
-conan install .. -s build_type=%1 -s compiler="Visual Studio" -s compiler.version=15 -s compiler.toolset=v141 -s arch=x86 -o gtest=1.10.0 --build=gtest
+conan install .. -s build_type=%1 -s compiler="Visual Studio" -s compiler.version=15 -s compiler.toolset=v141 -s arch=x86 -o gtest=1.10.0 --build=missing
 cmake .. -G "Visual Studio 15 2017" -A Win32 
 msbuild Patterns.sln /p:Configuration=%1 /p:Platform="Win32" 
-cd ..
+cd bin\%1
+PatternsTest
+cd ..\..\..
