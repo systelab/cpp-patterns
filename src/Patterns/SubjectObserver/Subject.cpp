8f6dd2e5ebc2715a941c58833a5efe77f0c01768
josepvilabadillo <jvila1@werfen.com>
2020-09-03 12:39:19 +0200

    Added declarations of Subject methods

--- src/Patterns/SubjectObserver/Subject.cpp
+++ src/Patterns/SubjectObserver/Subject.cpp
@@ -1,9 +1,22 @@
 #include "stdafx.h"
 
 #include "Subject.h"
+#include "Observer.h"
 
 namespace systelab { namespace patterns {
 	Subject::Subject()
+	{
+	}
+	
+	void Subject::attach(Observer*)
+	{
+	}
+	
+	void Subject::dettach(Observer*)
+	{
+	}
+	
+	void Subject::notify()
 	{
 	}
 }}
--- src/Patterns/SubjectObserver/Subject.h
+++ src/Patterns/SubjectObserver/Subject.h
@@ -1,10 +1,17 @@
 #pragma once
 
 namespace systelab { namespace patterns {
+	
+	class Observer;
+	
 	class Subject
 	{
 	public:
 		Subject();
+		
+		void attach(Observer*);
+		void dettach(Observer*);
+		void notify();
 	};
 	
 }}
